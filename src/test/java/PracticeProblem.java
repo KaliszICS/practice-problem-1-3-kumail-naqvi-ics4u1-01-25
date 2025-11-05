b55817e9b25cae0faab2687df6442a04a1cf72a9
kumail-naqvi-ics4u1-01-25 <kumail.naqvi@student.tdsb.on.ca>
2025-11-05 04:25:39 +0000

    updated 2

--- src/test/java/PracticeProblem.java
+++ src/test/java/PracticeProblem.java
@@ -1,37 +1,27 @@
-import java.io.*;
-
 public class PracticeProblem {
 
     public static void main(String[] args) {
     
     }
 
-
     public static String readFile(String filename) {
-        FileReader inputStream = null;
-        try {
-            inputStream = new FileReader(filename);
-            StringBuilder sb = new StringBuilder();
-            int c;
-            
-            while ((c = inputStream.read()) != -1) {
-                sb.append((char) c);
+        StringBuilder sb = new StringBuilder();
+        try (java.io.FileReader fr = new java.io.FileReader(filename)) {
+            int ch;
+            while ((ch = fr.read()) != -1) {
+                sb.append((char) ch);
             }
-            return sb.toString();
-        } catch (IOException e) {
+        } catch (java.io.IOException e) {
             return "";
-        } finally {
-            try {
-                if (inputStream != null) inputStream.close();
-            } catch (IOException e) {
-        
-            }
         }
+        return sb.toString();
     }
-
 
     public static String backwardsReadFile(String filename) {
-        String content = readFile(filename);
-        return new StringBuilder(content).reverse().toString();
+        String s = readFile(filename);
+        if (s.isEmpty()) return "";
+        return new StringBuilder(s).reverse().toString();
     }
+
+
 }
